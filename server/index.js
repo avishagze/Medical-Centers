6e9e1777fac037dc8a9366f6897b2aef31b825ac
avishag <avishagze@gmail.com>
2021-12-20 21:28:20 +0200

    changed to hello world2

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello world!" });
+    res.json({ message: "Hello world2!" });
   });
 
 app.listen(PORT, () => {
